6d0773f2190ad75ae1f7cf21ef97bcafe4c9562e
Silas Ferreira <silasferreira96@hotmail.com>
2018-11-01 06:39:01 -0300

    App01 - Concluido

---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using App01_ConsultarCEP.Servico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App01_ConsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Botao.Clicked += BuscarCep;
+        }
+
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            var cep = Cep.Text.Trim();
+
+            if (isValid(cep))
+            {
+                try {
+                    var endereco = ViaCepServico.BuscarEnderecoViapCep(cep);
+                    if (endereco == null)
+                    {
+                        DisplayAlert("ERRO", "Cep não encontrado !", "Ok");
+                        return;
+                    }
+
+                    Resultado.Text = string.Format("Endereço: {2} de {3} {0}, {1}", endereco.Localidade, endereco.Uf, endereco.Logradouro, endereco.Bairro);
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("ERRO CRÍTICO", e.Message, "Ok");
+                }
+            }
+            else
+            {
+                DisplayAlert("Erro", "Cep inválido !", "Ok");
+            }
+        }
+
+        private bool isValid(string cep)
+        {
+            if(cep.Length != 8)
+                return false;
+            if (int.TryParse(cep, out int novoCep))
+                return false;
+            return true;
+        }
+    }
+}
---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCepServico.cs
@@ -0,0 +1,19 @@
+﻿using App01_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace App01_ConsultarCEP.Servico
+{
+    public class ViaCepServico
+    {
+        public static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViapCep(string cep)
+        {
+            string newUrl = string.Format(EnderecoURL, cep);
+            var wc = new WebClient();
+            string downlaod = wc.DownloadString(newUrl);
+            return JsonConvert.DeserializeObject<Endereco>(downlaod) ?? null;
+        }
+    }
+}
